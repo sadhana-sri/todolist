2e5aaf4c70fbf7dab6db424ded25acda854263c0
sadhana-sri <ssadhanasri2020@gmail.com>
2025-08-30 19:10:04 +0530

    todolist.py

--- todolist.py
+++ todolist.py
@@ -1,1 +1,25 @@
-hello
+checklist=[]
+num_tasks=int(input("How many tasks do you want to add today?"))
+
+for i in range(num_tasks):
+    task=input(f"Enter task {i+1}:")
+    checklist.append(task)
+
+completed_tasks=[]
+incomplete_tasks=[]
+
+print("\n Now mark the completed tasks(yes/no):")
+for task in checklist:
+    status=input(f"Did you complete this task -{task}? (yes/no)")
+    if status=='yes':
+        completed_tasks.append(task)
+    else:
+        incomplete_tasks.append(task)
+
+print("\n Completed tasks:")
+for task in completed_tasks:
+    print("#"+task)
+
+print("\n Incomplete tasks:")
+for task in incomplete_tasks:
+    print("#"+task)
